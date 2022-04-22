d28cba827fe30ba3c0a8f838065fbffe51c00955
Alaska <luanabatista979@gmail.com>
2022-04-22 11:58:50 -0300

    Adição de comentários

--- udp_client.py
+++ udp_client.py
@@ -3,14 +3,14 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 questions = 4
 
+# Envia 
 for x in range(questions):
     message = str(input(f"Insira sua resposta: "))
     client.sendto(message.encode(), ('127.0.0.1', 9999))
 
+# Recebe 
 print('Resultado da correção:')
 for x in range(questions):
     recv_msg_bytes, address = client.recvfrom(1024)
     recv_msg_string = recv_msg_bytes.decode()
     print(f"{recv_msg_string}")
-
-client.close()

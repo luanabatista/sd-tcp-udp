ba02bb8b4803380037aaef383ec78095067202db
Alaska <luanabatista979@gmail.com>
2022-04-22 09:33:36 -0300

    Conserto da regra de negócio do UDP

--- udp_client.py
+++ udp_client.py
@@ -3,31 +3,21 @@
 #  O servidor devolve a msg (echo)
 
 import socket
+import threading
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+questions = 4
 #voce deseja corrigir quantas perguntas
 #for
 
-message = str(input("Insira <questão;alternativas;respostas>: "))
-client.sendto(message.encode(), ('127.0.0.1', 9999))
-recv_msg_bytes, address = client.recvfrom(1024)
-recv_msg_string = recv_msg_bytes.decode()
+for x in range(questions):
+    message = str(input(f"Resposta {x+1}:"))
+    client.sendto(message.encode(), ('127.0.0.1', 9999))
 
-message = str(input("Insira <número da questão>;<número alternativas>;<respostas>: "))
-client.sendto(message.encode(), ('127.0.0.1', 9999))
-recv_msg_bytes, address = client.recvfrom(1024)
-recv_msg_string = recv_msg_bytes.decode()
+for x in range(questions):
+    recv_msg_bytes, address = client.recvfrom(1024)
+    recv_msg_string = recv_msg_bytes.decode()
+    print(recv_msg_string)
 
-message = str(input("Insira <número da questão>;<número alternativas>;<respostas>: "))
-client.sendto(message.encode(), ('127.0.0.1', 9999))
-recv_msg_bytes, address = client.recvfrom(1024)
-recv_msg_string = recv_msg_bytes.decode()
-
-message = str(input("Insira <número da questão>;<número alternativas>;<respostas>: "))
-client.sendto(message.encode(), ('127.0.0.1', 9999))
-recv_msg_bytes, address = client.recvfrom(1024)
-recv_msg_string = recv_msg_bytes.decode()
-
-print(recv_msg_string)
 client.close()
--- udp_server.py
+++ udp_server.py
@@ -1,66 +1,92 @@
-# Servidor: fica em aguardo de requisição de algum cliente. 
-# Quando recebe uma requisição devolve uma mensagem de correção e encerra a conexão. 
 import socket
 
-questions = []
 class Question:
     def __init__(self, question, alternatives, answer):
         self.question = question
         self.alternatives = alternatives
         self.answer = answer
 
-question1 = Question(1, 4, 'VVVF')
-questions.append(question1)
-question2 = Question(2, 5, 'VFVFV')
-questions.append(question2)
-question3 = Question(3, 3, 'FFV')
-questions.append(question3)
-question4 = Question(4, 4, 'VFVV')
-questions.append(question4)
+class ClientRequest:
+    def __init__(self, address, message):
+        self.address = address
+        self.message = message
 
-#se existe uma questao com esse numero
-#para cada questao, se existe uma de mesmo numero, entao para cada alternativa verificar se esta igual
-def correction(number, alternatives, answer):
-    counter = 0
-    sucesses = 0
-    errors = 0
-    for question in questions:
-        if question.question == number:
-            for alternative in question.answer:
-                if alternative == answer[counter]:
-                    sucesses += 1
-                else:
-                    errors += 1
-                counter += 1
-    return [number, sucesses, errors]
+
+
 
 # Cria um Socket UDP
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(('127.0.0.1', 9999))
 print("Aguardando requisição...")
 
-while True: 
+requests = []
+connected_clients = []
+questions = []
+user_questions = []
+user_results = []
+
+question1 = Question(1, 4, 'VVVF')
+questions.append(question1)
+question2 = Question(2, 4, 'VFVF')
+questions.append(question2)
+question3 = Question(3, 4, 'FFVV')
+questions.append(question3)
+question4 = Question(4, 4, 'VFVV')
+questions.append(question4)
+
+
+def handle(address):
+    for client_request in requests: 
+        if client_request.address == address:
+            number = int(client_request.message[0])
+            alternatives = int(client_request.message[1])
+            answer = str(client_request.message[2])
+
+            for question in questions:
+                counter = 0
+                sucesses = 0
+                errors = 0
+                if question.question == number:
+                    for alternative in question.answer:
+                        if alternative == answer[counter]:
+                            sucesses += 1
+                        else:
+                            errors += 1
+                        counter += 1      
+                    server.sendto(f'Questão: {number}; Acertos: {sucesses}; Erros:{errors}'.encode('utf-8'), address)
+
+def count_client_requests(address):
+    counter = 0
+    for client_request in requests: 
+        if client_request.address == address:
+            counter += 1 
+    return counter
+
+def receive():
+    while True:
+        # procura no array se tem 4 itens deste endereço
+        client_message, address = server.recvfrom(1024)
+        print(f"Requisição recebida de: {str(address)}")
+        message = client_message.decode().split(";")
+        requests.append(ClientRequest(address, message))
+
+        if count_client_requests(address) == len(questions):
+            handle(address)
+            
+receive()      
+
+
+'''
+while True:        
     try:
-        message_bytes, address = server.recvfrom(1024)
-        message_str = message_bytes.decode()
-
-        print(f'Request recebido de: {address}. Requisição: ({message_str})')
-
-        message_array = message_str.split(";")
-        number = int(message_array[0])
-        alternatives = int(message_array[1])
-        answer = str(message_array[2])
-
-        
-        try:
-            resp = correction(number, alternatives, answer)
-            num_quest = resp[0]
-            sucesses = resp[1]
-            errors = resp[2]
+        resp = correction(number, alternatives, answer)
+        num_quest = resp[0]
+        sucesses = resp[1]
+        errors = resp[2]
                     
-            server.sendto(f'Questão: {num_quest}; Acertos: {sucesses}; Erros:{errors}'.encode('utf-8'), address)
-        except:
-            print(f'Erro no request recebido de: {address}.')
-            server.sendto("Erro ao realizar requisição.".encode('utf-8'), address)
+        server.sendto(f'Questão: {num_quest}; Acertos: {sucesses}; Erros:{errors}'.encode('utf-8'), address)
+    except:
+        print(f'Erro no request recebido de: {address}.')
+        server.sendto("Erro ao realizar requisição.".encode('utf-8'), address)
     except KeyboardInterrupt:
-        server.sendto("Serviço finalizado.............".encode('utf-8'), address)
+        server.sendto("Serviço finalizado.............".encode('utf-8'), address)'''
